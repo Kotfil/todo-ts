c794c295fd02094f2a7acbd8fc199a57a8ed79e2
Fil <filippkotenko@outlook.com>
2020-11-25 12:07:25 +0200

    Updates

---
+++ precache-manifest.a19ae929d6f4a5eb9e9a08c18ab69b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb6348cc27a62a418386c395e37b345",
+    "url": "/todo-ts/index.html"
+  },
+  {
+    "revision": "ccdb9eb2da204832ac24",
+    "url": "/todo-ts/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "cbe6b2b1b1d7437959ac",
+    "url": "/todo-ts/static/js/2.84da9dbd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-ts/static/js/2.84da9dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdb9eb2da204832ac24",
+    "url": "/todo-ts/static/js/main.3149dcbe.chunk.js"
+  },
+  {
+    "revision": "a316fd819c089a89d590",
+    "url": "/todo-ts/static/js/runtime-main.c14466b2.js"
+  }
+]);
